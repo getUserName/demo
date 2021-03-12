3a208a4061ce4fd311080c0d9f265360b445ebcc
mclozano <mclozano@courtesypoint.com>
2021-03-12 23:49:33 +0800

    s

--- src/main/java/com/example/controllers/MenuItemController.java
+++ src/main/java/com/example/controllers/MenuItemController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,14 @@ public class MenuItemController {
 	@GetMapping("/itemsbycategory")
 	@ResponseBody
 	public Set<MenuItem> getItemsByCategory(@RequestParam(value = "category") String name) {
-		System.out.println(name);
 		return repo.findByMenuCategoryName(name);
+	}
+	
+	@GetMapping("/menuitems")
+	@ResponseBody
+	public Set<MenuItem> getMenuItems() {
+		Set<MenuItem> items = new HashSet<>();
+		repo.findAll().forEach(i -> items.add(i));
+		return items;
 	}
 }
